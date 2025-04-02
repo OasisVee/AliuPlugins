0d4561cf8876a957d199c2f12f4235deb4de623c
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-04-02 13:32:29 -0400

    Update ReadAllGuilds.kt

--- ReadAllGuilds/src/main/kotlin/me/aniimalz/plugins/ReadAllGuilds.kt
+++ ReadAllGuilds/src/main/kotlin/me/aniimalz/plugins/ReadAllGuilds.kt
@@ -10,17 +10,17 @@ import com.discord.stores.StoreStream
 @AliucordPlugin
 class ReadAllGuilds : Plugin() {
     override fun start(ctx: Context) {
-        commands.registerCommand("readallguilds", "Mark all of your guilds as read. Will wait 5 seconds between actions to not spam api") {
+        commands.registerCommand("readallguilds", "Mark all of your guilds as read. Will wait 0.5 seconds between actions to not spam api") {
             Utils.threadPool.execute {
                 val guildIds = StoreStream.getGuilds().guilds.keys.toList()
                 var markedCount = 0
                 
                 guildIds.forEach { guildId ->
-                    // Mark guild as read
-                    StoreStream.getMessageAck().ackGuild(ctx, guildId)
+                    // Mark guild as read using the correct method
+                    StoreStream.getMessageAck().ackMessages(ctx, guildId, null)
                     markedCount++
                     
-                    // Sleep between requests to avoid API rate limits
+                    // Sleep between requests to avoid API rate limits (reduced to 500ms)
                     Thread.sleep(500)
                 }
             }
