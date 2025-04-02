468ac4ea4fac01bbc0e8dec707f5624523d3aded
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-04-02 14:15:55 -0400

    Update ReadAllGuilds.kt

--- ReadAllGuilds/src/main/kotlin/me/aniimalz/plugins/ReadAllGuilds.kt
+++ ReadAllGuilds/src/main/kotlin/me/aniimalz/plugins/ReadAllGuilds.kt
@@ -6,56 +6,43 @@ import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.api.CommandsAPI
 import com.aliucord.entities.Plugin
 import com.discord.stores.StoreStream
-import com.discord.api.channel.Channel
-import com.discord.models.domain.ModelReadState
-import com.discord.utilities.rest.RestAPI
 
 @AliucordPlugin
 class ReadAllGuilds : Plugin() {
     override fun start(ctx: Context) {
-        commands.registerCommand("readallguilds", "Mark all of your guilds as read. Will wait 0.5 seconds between actions to not spam api") {
+        commands.registerCommand("readallguilds", "Mark all of your guilds as read. Will wait 5 seconds between actions to not spam api") {
             Utils.threadPool.execute {
                 try {
-                    // Get list of all guild IDs
-                    val guildIds = StoreStream.getGuilds().guilds.keys.toList()
-                    val channelsToAck = mutableListOf<Map<String, Any>>()
+                    // Get the guilds collection
+                    val guilds = StoreStream.getGuilds().guilds.keys
                     
-                    // Collect all unread channels from all guilds
-                    for (guildId in guildIds) {
-                        val readStateStore = StoreStream.getReadStates()
-                        val guildChannels = StoreStream.getChannels().getGuildChannels(guildId)
-                        
-                        // Get all channels for this guild
-                        guildChannels?.forEach { channelId, _ ->
-                            // Check if channel has unread messages
-                            if (readStateStore.hasUnread(channelId)) {
-                                // Add to list of channels to acknowledge
-                                val lastMessageId = readStateStore.getLastMessageId(channelId)
-                                if (lastMessageId != null) {
-                                    channelsToAck.add(mapOf(
-                                        "channelId" to channelId,
-                                        "messageId" to lastMessageId,
-                                        "readStateType" to 0
-                                    ))
-                                }
+                    // Log the number of guilds being processed
+                    logger.info("Starting to mark ${guilds.size} guilds as read")
+                    
+                    // Try different approach based on available methods
+                    for (guildId in guilds) {
+                        try {
+                            // First approach: Try to call ackGuild with just the guild ID
+                            StoreStream.getMessageAck().ackGuild(guildId)
+                        } catch (e: NoSuchMethodError) {
+                            try {
+                                // Second approach: Try the ackGuild method without the callback
+                                StoreStream.getMessageAck().ackGuild(ctx, guildId)
+                            } catch (e: NoSuchMethodError) {
+                                logger.error("Could not find appropriate ackGuild method", e)
                             }
                         }
+                        
+                        // Wait 5 seconds between requests to avoid API rate limits
+                        Thread.sleep(5000)
                     }
                     
-                    // Dispatch a BULK_ACK event to mark all channels as read
-                    StoreStream.getDispatcher().dispatch(mapOf(
-                        "type" to "BULK_ACK",
-                        "context" to "APP",
-                        "channels" to channelsToAck
-                    ))
-                    
-                    logger.info("Marked ${channelsToAck.size} channels as read across ${guildIds.size} guilds")
-                    
+                    logger.info("Finished marking guilds as read")
                 } catch (e: Exception) {
-                    logger.error("Failed to mark guilds as read", e)
+                    logger.error("Error while marking guilds as read", e)
                 }
             }
-            CommandsAPI.CommandResult("Started marking guilds as read. This can take a while if you're in a lot of servers", null, false)
+            CommandsAPI.CommandResult("Marking all guilds as read. This can take a while if you're in a lot of servers", null, false)
         }
     }
 
